d197f041443eee90e584c62a46088a55d725ca31
salt-dev <salt-dev@admin-salts-MacBook-Pro.local>
2021-02-23 09:59:03 +0100

    pagination update

--- src/index.js
+++ src/index.js
@@ -1,85 +1,82 @@
-import './styles/main.scss'
+import './styles/main.scss';
 
-const submitButton = document.getElementById("search-button");
-const input = document.getElementById("search");
+const submitButton = document.getElementById('search-button');
+const input = document.getElementById('search');
 let page = 1;
-const key = 'sntlYzNOgnb_utk2xCDj-dsqX0BpDi4Tb5cOy2CuXbY'
 
-submitButton.addEventListener("click", (e) => {
+const createButtons = () => {
+  const pagination = document.querySelector('.pagination');
+  pagination.innerHTML = `<button onclick="renderPrevPage()" class="button--prev">Previous</button>
+  <button onclick="renderNextPage()" class="button--next">Next</button>`;
+};
+
+const renderPage = data => {
+  const figure = document.getElementById('img-container');
+  figure.innerHTML = '';
+  data.results.forEach(image => {
+    figure.innerHTML += `<img src="${image.urls.small}" alt="${image.description}">`;
+  });
+
+  if (data.total_pages === 1) {
+    return true;
+  } if (page === 1) {
+    createButtons();
+    const prevButton = document.querySelector('.button--prev');
+    prevButton.style.display = 'none';
+  } else if (page === data.total_pages) {
+    createButtons();
+    const nextButton = document.querySelector('.button--next');
+    nextButton.style.display = 'none';
+  } else {
+    createButtons();
+  }
+  return true;
+};
+
+submitButton.addEventListener('click', e => {
   e.preventDefault();
-  console.log(input.value)
-  console.log('hello')
   fetch(`https://api.unsplash.com/search/photos?page=${page}&query=${input.value}&orientation=squarish`, {
     method: 'GET',
     headers: {
-      "Authorization": "Client-ID sntlYzNOgnb_utk2xCDj-dsqX0BpDi4Tb5cOy2CuXbY"
-    }
+      Authorization: 'Client-ID sntlYzNOgnb_utk2xCDj-dsqX0BpDi4Tb5cOy2CuXbY',
+    },
   })
     .then(res => res.json())
     .then(data => {
-      console.log(data.total_pages)
-      renderPage(data)
+      renderPage(data);
     })
-    .catch((err) => {
-      console.error(err.message)
-    })
+    .catch(err => {
+      console.error(err.message);
+    });
 });
 
-
-
-const renderPage = (data) => {
-  const figure = document.getElementById("img-container");
-  figure.innerHTML = ''
-  data.results.forEach(image => {
-    figure.innerHTML += `<img src="${image.urls.small}" alt="${image.description}">`
-  });
-
-  const pagination = document.querySelector('.pagination');
-  if(page === 1) {
-    pagination.innerHTML = `<button onclick="renderPrevPage()" class="button--prev">Previous</button>
-    <button onclick="renderNextPage()" class="button--next">Next</button>`
-    const button = document.querySelector('.button--prev');
-    button.style.display = "none";
-  } else if (page === data.total_pages) {
-    pagination.innerHTML = `<button onclick="renderPrevPage()" class="button--prev">Previous</button>
-    <button onclick="renderNextPage()" class="button--next">Next</button>`
-    const button = document.querySelector('.button--next');
-    button.style.display = "none";
-  } else {
-    pagination.innerHTML = `<button onclick="renderPrevPage()" class="button--prev">Previous</button>
-    <button onclick="renderNextPage()" class="button--next">Next</button>`
-  }
-};
-
 const renderNextPage = () => {
-  page += 1
-  fetch(`https://api.unsplash.com/search/photos?page=${page}&query=${input.value}&orientation=squarish`,  {
+  page += 1;
+  fetch(`https://api.unsplash.com/search/photos?page=${page}&query=${input.value}&orientation=squarish`, {
     method: 'GET',
     headers: {
-      "Authorization": "Client-ID sntlYzNOgnb_utk2xCDj-dsqX0BpDi4Tb5cOy2CuXbY"
-    }
+      Authorization: 'Client-ID sntlYzNOgnb_utk2xCDj-dsqX0BpDi4Tb5cOy2CuXbY',
+    },
   })
     .then(res => res.json())
     .then(newData => {
-      renderPage(newData)
-    })
-};
-
-const renderPrevPage = () => {
-  page -= 1
-  fetch(`https://api.unsplash.com/search/photos?page=${page}&query=${input.value}&orientation=squarish`,  {
-    method: 'GET',
-    headers: {
-      "Authorization": "Client-ID sntlYzNOgnb_utk2xCDj-dsqX0BpDi4Tb5cOy2CuXbY"
-    }
-  })
-    .then(res => res.json())
-    .then(newData => {
-      renderPage(newData)
+      renderPage(newData);
     });
 };
 
-// window.run = run;
-// window.getCustomer = getCustomer;
+const renderPrevPage = () => {
+  page -= 1;
+  fetch(`https://api.unsplash.com/search/photos?page=${page}&query=${input.value}&orientation=squarish`, {
+    method: 'GET',
+    headers: {
+      Authorization: 'Client-ID sntlYzNOgnb_utk2xCDj-dsqX0BpDi4Tb5cOy2CuXbY',
+    },
+  })
+    .then(res => res.json())
+    .then(newData => {
+      renderPage(newData);
+    });
+};
+
 window.renderNextPage = renderNextPage;
 window.renderPrevPage = renderPrevPage;
