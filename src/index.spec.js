82c1b27af8b2492de1133f7614d9e2e8aa6ff3cb
salt-dev <salt-dev@admin-salts-MacBook-Pro.local>
2021-02-23 15:03:49 +0100

    testing commented out

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,9 +1,9 @@
-import './index.js'
-import './template.html'
+// import './index.js'
+// import './template.html'
 
-describe('The render page', () => {
-  it('should return true if only 1 page', () => {
-    const mockData = { total_pages: 1 };
-    expect(renderPage(mockData)).toEqual(true);
-  });
-});
+// describe('The render page', () => {
+//   it('should return true if only 1 page', () => {
+//     const mockData = { total_pages: 1 };
+//     expect(renderPage(mockData)).toEqual(true);
+//   });
+// });
