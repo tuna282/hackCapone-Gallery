b7f75f69b4f602c9dd76686b1d39b9e99e08d280
salt-dev <salt-dev@admin-salts-MacBook-Pro.local>
2021-02-23 11:28:10 +0100

    example

---
+++ src/index.spec.js
@@ -0,0 +1,9 @@
+import './index.js'
+import './template.html'
+
+describe('The render page', () => {
+  it('should return true if only 1 page', () => {
+    const mockData = { total_pages: 1 };
+    expect(renderPage(mockData)).toEqual(true);
+  });
+});
